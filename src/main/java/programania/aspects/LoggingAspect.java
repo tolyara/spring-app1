d6c132a5b8e7880bb5cd92029a6d65260b3ac0cb
Anatoliy <tolyara1961@gmail.com>
2023-02-11 22:49:26 +0200

    method signature params;

--- src/main/java/programania/aspects/LoggingAspect.java
+++ src/main/java/programania/aspects/LoggingAspect.java
@@ -6,6 +6,7 @@ import org.aspectj.lang.annotation.Before;
 import org.aspectj.lang.reflect.MethodSignature;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import programania.Book;
 
 @Component
 @Aspect
@@ -15,12 +16,26 @@ public class LoggingAspect {
     @Before("programania.pointcuts.PointCut.allAddMethods()")
     public void beforeAddLoggingAdvice(JoinPoint jp) {
         MethodSignature methodSignature = (MethodSignature) jp.getSignature();
-        System.out.println("methodSignature = " + methodSignature);
-        System.out.println("getMethod = " + methodSignature.getMethod());
-        System.out.println("getReturnType = " + methodSignature.getReturnType());
-        System.out.println("getName = " + methodSignature.getName());
+//        System.out.println("methodSignature = " + methodSignature);
+//        System.out.println("getMethod = " + methodSignature.getMethod());
+//        System.out.println("getReturnType = " + methodSignature.getReturnType());
+//        System.out.println("getName = " + methodSignature.getName());
 
-        System.out.println("Logging of getting a book");
+        if (methodSignature.getName().equals("addBook")) {
+            Object[] args = jp.getArgs();
+            for (Object o : args) {
+                if (o instanceof Book) {
+                    Book book = (Book) o;
+                    System.out.print("name = " + book.getName() + ", ");
+                    System.out.print("author = " + book.getAuthor() + ", ");
+                    System.out.println("year = " + book.getYear());
+                } else if (o instanceof String) {
+                    System.out.println("librarian = " + o);
+                }
+            }
+        }
+
+        System.out.println("Logging of adding a book/magazine");
     }
 
 //    @Before("programania.pointcuts.PointCut.allGetMethods()")
