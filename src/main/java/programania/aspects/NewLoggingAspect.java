1ea743dd371e172da28767a03c34a54164899808
Anatolii <anatolii.melchenko@academysmart.com>
2023-02-14 01:13:40 +0200

    @Around, with ProceedingJoinPoint;

--- src/main/java/programania/aspects/NewLoggingAspect.java
+++ src/main/java/programania/aspects/NewLoggingAspect.java
@@ -1,5 +1,6 @@
 package programania.aspects;
 
+import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
@@ -9,8 +10,17 @@ import org.springframework.stereotype.Component;
 public class NewLoggingAspect {
 
     @Around("execution(public String returnBook())")
-    public void aroundReturnBookLoggingAdvice() {
-        System.out.println("Around logging of returning a book");
+    public Object aroundReturnBookLoggingAdvice(ProceedingJoinPoint proceedingJoinPoint) throws Throwable {
+        System.out.println("Around logging before returning a book");
+
+        long begin = System.currentTimeMillis();
+        Object targetMethodResult = proceedingJoinPoint.proceed();
+//        Object targetMethodResult = "Book2";
+        long end = System.currentTimeMillis();
+
+        System.out.println("Around logging after returning a book");
+        System.out.println("Time of work " + (end - begin) + " millis");
+        return targetMethodResult;
     }
 
 }
