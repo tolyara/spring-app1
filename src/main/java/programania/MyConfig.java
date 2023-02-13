efb100718ba327d874c0f7881aa698529344247b
Anatolii <anatolii.melchenko@academysmart.com>
2023-02-14 00:53:29 +0200

    @Primary in config file;

--- src/main/java/programania/MyConfig.java
+++ src/main/java/programania/MyConfig.java
@@ -1,11 +1,24 @@
 package programania;
 
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.context.annotation.*;
+import programania.entity.TestService;
+import programania.entity.TestServiceImpl;
+import programania.entity.TestServiceImpl2;
 
 @Configuration
 @ComponentScan("programania")
 @EnableAspectJAutoProxy
 public class MyConfig {
+
+//    @Bean
+//    public TestService testService() {
+//        return new TestServiceImpl();
+//    }
+//
+//    @Bean
+//    @Primary
+//    public TestService testService2() {
+//        return new TestServiceImpl2();
+//    }
+
 }
